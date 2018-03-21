178cb75cc4ac64c775b0139a2c720d751c36a548
叶金寿 <aquariusye@163.com>
2018-03-21 11:31:21 +0800

    diff time

--- controlPanel/controlPanel.go
+++ controlPanel/controlPanel.go
@@ -423,48 +423,54 @@ func disconnectPeer(hash string) {
 		Controller.Disconnect(hash)
 	}
 }
-
-
+var arr = [...]ConnectionInfo{
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"120.24.173.245", MomentConnected:time.Now().Add(-time.Minute*121)},
+		Hash:"106aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd1",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"123.57.222.175", MomentConnected:time.Now().Add(-time.Minute*5)},
+		Hash:"206aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd2",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"119.23.220.132", MomentConnected:time.Now().Add(-time.Hour*5)},
+		Hash:"306aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"123.56.194.16", MomentConnected:time.Now().Add(-time.Minute*15)},
+		Hash:"406aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd1",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"39.108.117.150", MomentConnected:time.Now().Add(-time.Second*9)},
+		Hash:"506aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd2",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"120.77.174.44", MomentConnected:time.Now().Add(-time.Minute*10)},
+		Hash:"606aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"39.108.212.239", MomentConnected:time.Now().Add(-time.Minute*60)},
+		Hash:"706aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd1",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"216.58.216.164", MomentConnected:time.Now().Add(-time.Second*600)},
+		Hash:"806aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd2",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"59.37.96.63", MomentConnected:time.Now().Add(-time.Minute*85)},
+		Hash:"906aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"159.87.96.163", MomentConnected:time.Now().AddDate(0,0,-2)},
+		Hash:"936aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
+	},
+	{
+		Connection:p2p.ConnectionMetrics{PeerAddress:"59.137.196.93", MomentConnected:time.Now().AddDate(0,0,-1)},
+		Hash:"946aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
+	},
+}
 func fake_peer() func(*ConnectionInfo) bool {
 	idx := 0
-	arr := [...]ConnectionInfo{
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"120.24.173.245", MomentConnected:time.Now()},
-			Hash:"106aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd1",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"123.57.222.175", MomentConnected:time.Now().Add(time.Second*2)},
-			Hash:"206aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd2",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"119.23.220.132", MomentConnected:time.Now().Add(time.Second*5)},
-			Hash:"306aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"123.56.194.16", MomentConnected:time.Now().Add(time.Second*7)},
-			Hash:"406aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd1",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"39.108.117.150", MomentConnected:time.Now().Add(time.Second*9)},
-			Hash:"506aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd2",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"120.77.174.44", MomentConnected:time.Now().Add(time.Second*15)},
-			Hash:"606aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"39.108.212.239", MomentConnected:time.Now().Add(time.Second*22)},
-			Hash:"706aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd1",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"216.58.216.164", MomentConnected:time.Now().Add(time.Second*32)},
-			Hash:"806aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd2",
-		},
-		{
-			Connection:p2p.ConnectionMetrics{PeerAddress:"59.37.96.63", MomentConnected:time.Now().Add(time.Second*45)},
-			Hash:"906aed40def99ba4b8c77eb2e3b002bf04437781e6385fbb7a877ef410c7cbd3",
-		},
-	}
 	return func(info *ConnectionInfo) bool {
 		i := idx
 		if idx >= len(arr) {
@@ -475,6 +481,9 @@ func fake_peer() func(*ConnectionInfo) bool {
 
 		v2 := &arr[i]
 		info.Hash, info.Connection.PeerAddress = v2.Hash, v2.Connection.PeerAddress
+		info.Connection.MomentConnected = v2.Connection.MomentConnected
+		info.ConnectionTimeFormatted = FormatDuration(v2.Connection.MomentConnected)
+
 		return true
 	}
 }
@@ -482,11 +491,10 @@ func fake_peer() func(*ConnectionInfo) bool {
 func getPeers() []byte {
 	connections := AllConnections.SortedConnections()
 	//var fake_connections ConnectionInfoArray
-	fake_connections := connections
-
+	fake_connections := connections[:]
 	faker := fake_peer()
 	for {
-		if len(fake_connections) == 0 {
+		if len(connections) == 0 {
 			break
 		}
 		for _, v := range connections {
